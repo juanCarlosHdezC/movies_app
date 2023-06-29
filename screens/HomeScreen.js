f5a57cc83eb340473220efd73517aef94eabffe0
Juan Carlos Hernández <juan_hdezc@outlook.es>
2023-06-28 23:12:05 -0600

    Actualizacion

---
+++ screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const HomeScreen = () => {
+
+    return (
+      <View className="flex-1 bg-neutral-800">
+          <Text>HomeScreen</Text>
+      </View>
+      
+    )
+  
+}
+
+export default HomeScreen;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app.js", "./screens/HomeScreen.js","./navigation/AppNavigation.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
